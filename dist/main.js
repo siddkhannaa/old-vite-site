9b33cb84d02bc62ebeb8b65acd0d100330c16287
ide-k <siddharthkhanna2k@gmail.com>
2022-03-03 02:05:38 -0500

    this is incredibly frustrating

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 // npm run dev to start 
+// npm run deploy to update gh-pages
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
